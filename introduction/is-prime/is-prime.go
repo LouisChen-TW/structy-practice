f0fcac5b61ef577b2ae3fe4a96023fafeeef74ab
Louis <yue841206@gmail.com>
2024-02-29 13:18:39 +0800

    isPrime check

---
+++ introduction/is-prime/is-prime.go
@@ -0,0 +1,31 @@
+// Write a function, isPrime, that takes in a number as an argument. The function should return a boolean indicating whether or not the given number is prime.
+
+// A prime number is a number that is only divisible by two distinct numbers: 1 and itself.
+
+// For example, 7 is a prime because it is only divisible by 1 and 7. For example, 6 is not a prime because it is divisible by 1, 2, 3, and 6.
+
+// You can assume that the input number is a positive integer.
+
+package main
+
+import "math"
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	isPrime(2)  // -> true
+	isPrime(3)  // -> true
+	isPrime(6)  // -> false
+	isPrime(7)  // -> true
+	isPrime(25) // -> false
+}
